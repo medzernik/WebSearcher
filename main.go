0db5e509f218796c8062c740560291d4781b79e9
medzernik <medzernik1@gmail.com>
2022-06-16 19:57:21 +0200

    basic functionality

--- main.go
+++ main.go
@@ -3,12 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hello")
 	c := colly.NewCollector(
 		colly.AllowedDomains("en.wikipedia.org"),
 	)
 
+	var inputText string
+	contains := "web scraping"
+	//contains := os.Args[0]
+
+	// Find and print all links
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		inputText = fmt.Sprintf(e.Text)
+		output := strings.Count(inputText, contains)
+		fmt.Println(e.Name, output)
+	})
+
+	err := c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+func ContainsTextBool(input, search string) bool {
+	foundText := strings.Contains(search, input)
+	return foundText
 }
